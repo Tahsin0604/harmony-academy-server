c9e8e851696ee0679e7c0858521e4731e7b9be50
Natik Alam Tahsin <natikalamtahsin@gmail.com>
2023-06-14 01:32:26 +0600

    instructor api

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 const jwt = require("jsonwebtoken");
-
+const stripe = require("stripe")(process.env.STRIPE_SECRET);
 //
 const app = express();
 const port = process.env.PORT || 5000;
@@ -108,7 +108,6 @@ async function run() {
       if (existingUser) {
         return res.send({ message: "User Already exists" });
       }
-      console.log(user);
       const result = await usersCollections.insertOne(user);
       res.send(result);
     });
@@ -121,6 +120,56 @@ async function run() {
       const user = await usersCollections.findOne(query);
       const role = { role: user?.role };
       res.send(role);
+    });
+    //for instructor
+    app.get(
+      "/enrolled-users/:email",
+      verifyJwt,
+      verifyInstructor,
+      async (req, res) => {
+        const email = req.params.email;
+        if (req.decoded.email !== email) {
+          return res
+            .status(403)
+            .send({ error: true, message: "Forbidden Access" });
+        }
+        const result = await classesCollections
+          .aggregate([
+            {
+              $match: { instructorEmail: email },
+            },
+            {
+              $lookup: {
+                from: enrolledClasses,
+                localField: _id,
+                foreignField: classId,
+                as: enrollments,
+              },
+            },
+            {
+              $lookup: {
+                from: users,
+                localField: studentEmail,
+                foreignField: email,
+                as: users,
+              },
+            },
+            {
+              $project: {
+                _id: 1,
+                className: 1,
+                users: 1,
+              },
+            },
+          ])
+          .toArray();
+        res.send(result);
+      }
+    );
+    //for admin
+    app.get("/all-users", verifyJwt, verifyAdmin, async (req, res) => {
+      const result = await usersCollections.find().toArray();
+      res.send(result);
     });
     /* */
 
@@ -190,7 +239,7 @@ async function run() {
       const id = req.params.id;
       const instructors = await usersCollections
         .aggregate([
-          { $match: { role: "Instructor" } },
+          { $match: { role: "instructor" } },
           {
             $lookup: {
               from: "classes",
@@ -261,6 +310,26 @@ async function run() {
         .toArray();
       res.send(result);
     });
+    app.patch("/classes/:id", verifyJwt, verifyInstructor, async (req, res) => {
+      const id = req.params.id;
+      const body = req.body;
+      const query = { _id: new ObjectId(id) };
+      const result = await classesCollections.replaceOne(query, body);
+      console.log(result);
+      res.send(result);
+    });
+    app.get("/classes/:id", verifyJwt, verifyInstructor, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await classesCollections.findOne(query);
+      console.log(result);
+      res.send(result);
+    });
+    app.post("/classes", verifyJwt, verifyInstructor, async (req, res) => {
+      const body = req.body;
+      const result = await classesCollections.insertOne(body);
+      res.send(result);
+    });
     app.get("/classes-count", async (req, res) => {
       const result = await classesCollections.countDocuments({
         status: "approved",
@@ -278,17 +347,25 @@ async function run() {
           .send({ error: true, message: "Forbidden Access" });
       }
       const result = await selectedClassesCollections
-        .find({ email: email })
+        .find({ studentEmail: email })
         .toArray();
       res.send(result);
     });
-    app.post("/selectedClasses", verifyJwt, async (req, res) => {
-      const selectedClass = req.body;
-      if (req.decoded.email !== email) {
-        return res
-          .status(403)
-          .send({ error: true, message: "Forbidden Access" });
+    app.post("/selectedClasses", async (req, res) => {
+      const email = req.query.email;
+      const id = req.query.id;
+      const query = { $and: [{ classId: id }, { studentEmail: email }] };
+      const classExist = await selectedClassesCollections.findOne(query);
+      const enrolledExist = await enrolledClassesCollections.findOne(query);
+
+      if (classExist) {
+        return res.send({ exist: "Already selected" });
       }
+      if (enrolledExist) {
+        return res.send({ exist: "Already enrolled" });
+      }
+      const selectedClass = req.body;
+
       const result = await selectedClassesCollections.insertOne(selectedClass);
       res.send(result);
     });
@@ -300,6 +377,7 @@ async function run() {
     });
     app.get("/enrolledClasses", verifyJwt, async (req, res) => {
       const email = req.query.email;
+      const sortType = req.query.sortType || "asc";
       if (!email) {
         return res.send([]);
       }
@@ -308,39 +386,109 @@ async function run() {
           .status(403)
           .send({ error: true, message: "Forbidden Access" });
       }
-      const result = await enrolledClassesCollections.find(email).toArray();
+      let sort = {
+        paidAt: 1,
+      };
+      if (sortType !== "asc") {
+        sort = {
+          paidAt: -1,
+        };
+      }
+      const query = { studentEmail: email };
+      const result = await enrolledClassesCollections
+        .find(query)
+        .sort(sort)
+        .toArray();
+      res.send(result);
+    });
+    app.get("/enrolledClasses/:id", verifyJwt, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await enrolledClassesCollections.findOne(query);
+      res.send(result);
+    });
+    app.patch("/availabe-classes", async (req, res) => {
+      const classId = req.query.classId;
+      const selectedClassId = req.query.selectedClassId;
+      const queryClass = { _id: new ObjectId(classId) };
+      const querySelected = { _id: new ObjectId(selectedClassId) };
+      const option = { projection: { _id: 0, availableSeats: 1 } };
+      const result = await classesCollections.findOne(queryClass, option);
+      if (result.availableSeats < 1) {
+        const updatedDocs = {
+          $set: {
+            status: "Filled Up",
+          },
+        };
+        const updatedResult = await selectedClassesCollections.updateOne(
+          querySelected,
+          updatedDocs
+        );
+        return res.send(updatedResult);
+      }
       res.send(result);
     });
     // api only for instructor
-    app.get("/my-classes", verifyJwt, verifyInstructor, async (req, res) => {
-      const email = req.params.email;
-      if (req.decoded.email !== email) {
-        return res
-          .status(403)
-          .send({ error: true, message: "Forbidden Access" });
+    app.get(
+      "/my-classes/:email",
+      verifyJwt,
+      verifyInstructor,
+      async (req, res) => {
+        const email = req.params.email;
+        if (req.decoded.email !== email) {
+          return res
+            .status(403)
+            .send({ error: true, message: "Forbidden Access" });
+        }
+        const result = await classesCollections
+          .find({ instructorEmail: email })
+          .toArray();
+        res.send(result);
       }
-      const result = await classesCollections.find({ email: email }).toArray();
-      res.send(result);
-    });
+    );
     //api only for admin
-    app.get("/allclasses", verifyJwt, verifyAdmin, async (req, res) => {
-      const email = req.params.email;
-      if (req.decoded.email !== email) {
-        return res
-          .status(403)
-          .send({ error: true, message: "Forbidden Access" });
-      }
-      const result = await classesCollections.find({ email: email }).toArray();
+    app.get("/all-classes", verifyJwt, verifyAdmin, async (req, res) => {
+      const result = await classesCollections.find().toArray();
       res.send(result);
     });
     /* */
 
     /* Payment Api */
+    app.post("/create-payment-intent", verifyJwt, async (req, res) => {
+      const { amount } = req.body;
+      const amountPay = amount * 100;
+      // Create a PaymentIntent with the order amount and currency
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amountPay,
+        currency: "usd",
+        payment_method_types: ["card"],
+      });
+
+      res.send({
+        clientSecret: paymentIntent.client_secret,
+      });
+    });
     app.post("/payment", verifyJwt, async (req, res) => {
-      const body = req.params.body;
-      console.log(body);
+      const id = req.query.id;
+      const query = { _id: new ObjectId(id) };
+      const body = req.body;
+      const updateClassQuery = { _id: new ObjectId(req.body.classId) };
+      const updatedDocs = {
+        $inc: {
+          EnrolledStudents: 1,
+          availableSeats: -1,
+        },
+      };
+      const insertedResult = await enrolledClassesCollections.insertOne(body);
+      const deletedResult = await selectedClassesCollections.deleteOne(query);
+      const updatedResult = await classesCollections.updateOne(
+        updateClassQuery,
+        updatedDocs
+      );
+      res.send({ insertedResult, deletedResult, updatedResult });
     });
     /* */
+
     /*Reviews api */
     app.get("/reviews", async (req, res) => {
       const result = await reviewsCollections.find().toArray();
